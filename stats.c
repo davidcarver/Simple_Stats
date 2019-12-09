6da559684e4e5a72dbc68e57fcd48a32076f5ecd
David Carver <dcarver@master.frontera.tacc.utexas.edu>
2019-12-09 12:50:26 -0600

    Fix typos

--- stats.c
+++ stats.c
@@ -1,6 +1,6 @@
 /*
  * C program to input a infinate number of real numbers from stdin and find the
- * mean, variance and standard deviation with out using any arraies.
+ * mean, variance and standard deviation with out using any arrays.
  *
  * To build: gcc -Wall -O3 -o stats stats.c -lm
  * To run: cat data | ./stats
